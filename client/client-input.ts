63d7139942cb9728c7218996232f1d2c948a6b0b
Gustavo Kazuya Oshiro <gustavo@vizir.com.br>
2021-06-06 21:42:39 -0300

    refactor client side

---
+++ client/client-input.ts
@@ -0,0 +1,72 @@
+import { Engine, Vector } from "excalibur";
+import { KeyEvent, Keys } from "excalibur/dist/Input/Keyboard";
+import {
+  PointerDownEvent,
+  PointerUpEvent,
+} from "excalibur/dist/Input/PointerEvents";
+import { InputType } from "../shared/enums/input-type";
+
+export class ClientInput {
+  private readonly listeners: ((input: InputType) => void)[] = [];
+  private lastPointerPosition?: Vector;
+
+  public constructor(engine: Engine) {
+    engine.input.keyboard.on("press", this.onKeyPress.bind(this));
+    engine.input.pointers.primary.on("down", this.onPointerDown.bind(this));
+    engine.input.pointers.primary.on("up", this.onPointerUp.bind(this));
+  }
+
+  public addListener(callback: (input: InputType) => void) {
+    this.listeners.push(callback);
+  }
+
+  private onKeyPress(event: KeyEvent) {
+    const keyMap: {
+      [K in Keys]?: InputType;
+    } = {
+      [Keys.W]: InputType.up,
+      [Keys.A]: InputType.left,
+      [Keys.S]: InputType.down,
+      [Keys.D]: InputType.right,
+    };
+
+    const input = keyMap[event.key];
+
+    if (input !== undefined) {
+      this.listeners.forEach((listener) => listener(input));
+    }
+  }
+
+  private onPointerDown(event: PointerDownEvent) {
+    this.lastPointerPosition = event.pos;
+  }
+
+  private onPointerUp(event: PointerUpEvent) {
+    if (this.lastPointerPosition === undefined) {
+      return;
+    }
+
+    const xDiff = event.pos.x - this.lastPointerPosition.x;
+    const yDiff = event.pos.y - this.lastPointerPosition.y;
+
+    if (Math.abs(xDiff) >= Math.abs(yDiff)) {
+      if (xDiff >= 0) {
+        this.listeners.forEach((listener) => listener(InputType.right));
+
+        return;
+      }
+
+      this.listeners.forEach((listener) => listener(InputType.left));
+
+      return;
+    }
+
+    if (yDiff >= 0) {
+      this.listeners.forEach((listener) => listener(InputType.down));
+
+      return;
+    }
+
+    this.listeners.forEach((listener) => listener(InputType.up));
+  }
+}
---
+++ client/game-client.ts
@@ -0,0 +1,22 @@
+import { Color, DisplayMode, Engine } from "excalibur";
+import { ClientInput } from "./client-input";
+import { GameScene } from "./scenes/game-scene";
+import { SocketClient } from "./socket-client";
+
+const socketClient = new SocketClient();
+
+const engine = new Engine({
+  backgroundColor: Color.Black,
+  displayMode: DisplayMode.FullScreen,
+});
+
+const gameScene = new GameScene(engine, socketClient);
+socketClient.addOnPingListener(gameScene.receiveUpdate.bind(gameScene));
+
+const clientInput = new ClientInput(engine);
+clientInput.addListener(socketClient.sendInput.bind(socketClient));
+
+engine.addScene("game", gameScene);
+engine.goToScene("game");
+
+engine.start();
