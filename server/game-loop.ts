f11e4a588172ea92f90c9c5790548f9eebcce0cb
Gustavo Kazuya Oshiro <gustavo@vizir.com.br>
2021-06-09 20:37:41 -0300

    change server tickrate to 5

--- server/game-loop.ts
+++ server/game-loop.ts
@@ -13,7 +13,7 @@ export class GameLoop {
 
     this.serverInterval = setInterval(() => {
       this.listeners.forEach((listener) => listener());
-    }, 100);
+    }, 200);
   }
 
   public stopLoop() {
